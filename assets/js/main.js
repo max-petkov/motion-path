34725773711ab9a7fbf08d10c57b0054e863954f
unknown <maxy.dp@abv.bg>
2022-01-13 20:48:40 +0200

    added title animation and spheres pics

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,30 @@ spheres.forEach((sphere, i) => {
       "<"
     );
 });
+
+//Js - Heading animation
+const heroTitle = document.querySelector(".hero__title");
+
+function splitWord(container, text) {
+  const splitText = text.split(" ").map((word, i) => {
+    return `<span class="o-hidden hero__word-container"><span class="hero__word">${word}&nbsp;</span></span>`;
+  });
+
+  container.innerHTML = splitText.join("");
+}
+
+splitWord(heroTitle, heroTitle.textContent);
+gsap.to(".hero__word", {
+  delay: 0.3,
+  y: "-4px",
+  ease: Power4.easeOut,
+  duration: 0.5,
+  stagger: 0.03,
+});
+gsap.to(".hero__word", {
+  delay: 0.3,
+  autoAlpha: 1,
+  ease: Power4.easeOut,
+  duration: 1,
+  stagger: 0.03,
+});
